887f1e8712cf766f33f04bcab88fc3f635eedd40
yaron2148 <nahshonya@gmail.com>
2021-07-23 18:00:05 +0300

    delete "client" from this package

--- client.js
+++
@@ -1,8 +0,0 @@
-// FILE /client/chat.js
-
-// IMPORTANT! By default, socket.io() connects to the host that
-// served the page, so we dont have to pass the server url
-var socket = io.connect();
-socket.on('tmunat-matzav', (tmutza) => {
-  console.log('tmunat-matzav from server', tmutza);
-});
